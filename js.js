207426f65b2add23bc296358975536d2482db3f0
BLAL1111 <llaaghsthqq@gmail.com>
2024-11-13 11:54:20 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+//    String
+//    Number
+//    Boolean
+//    undefined
+//    null
+//    Object
+// let name = prompt('what is your name')
+// console.log(`hello master ${name} get in wibsite`)
+// document.write(`hello master ${name} get in wibsite`)
+// let num = 5;
+// let num2 = 4;
+// console.log(num + num2); //جمع
+// console.log(num - num2); //طرح
+// console.log(num * num2); //ضرب
+// console.log(num ** num2); //أص
+// console.log(num / num2); //قصمة
+// console.log(num % num2); //مد
+// let acont = prompt("معاك كام يا حلو")
+// let y = 'ج'
+// console.log(0.10 * acont + y)
+let ashi = '01 '
+document.write(ashi.repeat(3000))
+
+
